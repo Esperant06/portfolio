280a1b2e9dc142da2c848d5ba30f75f523fbc919
Esperant <d.esperant6@gmail.com>
2026-02-09 13:47:11 +0100

    petite modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Contact } from "lucide-react";
 import { Navbar } from "./layout/Navbar";
 import { About } from "./sections/About";
+import { Contact } from "./sections/Contact";
 import { Experience } from "./sections/Experience";
 import { Hero } from "./sections/Hero";
 import { Projects } from "./sections/Projects";
