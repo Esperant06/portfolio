072666e546a6b77d6a3d51be1019c28b69cf1d93
Esperant <d.esperant6@gmail.com>
2026-02-09 08:52:03 +0100

    import sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
+import { Contact } from "lucide-react";
 import { Navbar } from "./layout/Navbar";
+import { About } from "./sections/About";
+import { Experience } from "./sections/Experience";
+import { Hero } from "./sections/Hero";
+import { Projects } from "./sections/Projects";
+import { Testimonials } from "./sections/Testimonials";
 
 function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
-      <main></main>
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonials />
+        <Contact />
+      </main>
     </div>
   );
 }
