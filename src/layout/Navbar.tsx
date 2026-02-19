b63e7b25df1efbfd9897966bf239599d0775ab7f
Esperant <d.esperant6@gmail.com>
2026-02-19 21:38:11 +0100

    effet lors de scroll de la page dans le menu

--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 import logo from "@/assets/logo.png";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ButtonMenu from "../components/ButtonMenu";
 
 const NavLinks = [
@@ -11,8 +11,20 @@ const NavLinks = [
 ]
 export const Navbar = () => {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () =>{
+            setIsScrolled(window.scrollY > 50);
+        }
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    })
+
     return (
-        <header className="fixed top-0 left-0 right-0 bg-transparent px-5 z-50">
+        <header className={`fixed top-0 left-0 right-0 ${isScrolled ? "glass-strong py-3" : "bg-transparent py-5"} z-50`}>
             <nav className="container mx-auto px-6 flex items-center justify-between h-16">
                 <a href="#">
                     <img src={logo} alt="Logo" className="h-10 mr-4" />
